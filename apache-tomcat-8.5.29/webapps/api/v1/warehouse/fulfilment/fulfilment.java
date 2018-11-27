d887f66de1038d81d9f3b8ed7b3002ac34bc1178
Edward <eingle101@gmail.com>
2018-11-27 22:12:44 +1100

    Offloaded fulfilment functionality from fulfilmentclient to fulfilment class

--- apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/fulfilment.java
+++ apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/fulfilment.java
@@ -6,7 +6,88 @@ import org.json.simple.parser.JSONParser;
 
 public class Fulfilment
 {
-	public Order[] returnOrders() throws Exception
+	//
+	public String getFulfilment(String orderInput)
+	{
+		//Returns string of all unfulfilled orders.
+		try
+		{
+			//Retrieves Products
+			Product[] getProducts = returnProducts();
+			//Retrieves Orders
+			Order[] getOrders = returnOrders();
+			//Retrieves argument orderId's as string array.
+			String[] orderIdInput = orderInput.split(",");
+			//Assumes input orders are valid.
+			
+			//For each order id input, processes order and retrieves orders that cannot be fulfilled.
+			ArrayList<Order> unfulfillableOrders = new ArrayList<Order>();
+			
+			for(int i = 0; i < orderIdInput.length; i++)
+			{
+				//Assumes the input orderId's to be present in orders, and that all orders have unique IDs
+				for(int i1 = 0; i1 < getOrders.length; i1++)
+				{
+					if(getOrders[i1].getOrderId() == Long.parseLong(orderIdInput[i]))
+					{
+						//Decrements all stored products by order.
+						//Retrieves each item in order.
+						Item[] orderItems = getOrders[i].getItems();
+						for(int i2 = 0; i2 < orderItems.length; i2++){
+							//Retrives product ids, decrements products by product ID's.
+							//First checks for orders.
+							for(int i3 = 0; i3 < getProducts.length; i3++){
+								//Retrives product ids, decrements products by product ID's.
+								if(orderItems[i2].getProductId() == getProducts[i3].getProductId()){
+									if(getProducts[i3].getQuantityOnHand() < orderItems[i2].getQuantity())
+									{
+										getOrders[i1].setStatus("Error: Unfulfillable");
+									}
+								}
+							}
+							if(getOrders[i1].getStatus().equals("Pending")){
+								//If order is fulfilled, decrements product quantities.
+								for(int i3 = 0; i3 < getProducts.length; i3++){
+									//Retrives product ids, decrements products by product ID's.
+									if(orderItems[i2].getProductId() == getProducts[i3].getProductId()){
+										getProducts[i3].setQuantityOnHand( getProducts[i3].getQuantityOnHand() - orderItems[i2].getQuantity() );
+										//TODO: Checks reorder threshold, places reorder if productQuantity is less than order quantity.
+										if(getProducts[i3].getQuantityOnHand() < getProducts[i3].getReorderThreshold())
+										{
+											ReorderStub newReorder = new ReorderStub(getProducts[i3].getProductId(), getProducts[i3].getReorderAmount());
+										}
+									}
+								}
+								//
+								getOrders[i1].setStatus("Fulfilled");
+							}
+						}
+					}
+				}
+			}
+			String returnString = "";
+			
+			for(int i = 0; i < getOrders.length; i++){
+				if(getOrders[i].getStatus().equals("Error: Unfulfillable")){
+					returnString += Long.toString(getOrders[i].getOrderId());
+					returnString +=",";
+				}
+				
+			}
+			//Deletes last ","
+			if(!(returnString.equals("")))
+			{
+				returnString = returnString.substring(0, returnString.length() - 1);
+			}
+			return returnString;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return "";
+	}
+	private Order[] returnOrders() throws Exception
 	{
 		//Returns all orders stored in data.json as Order objects
 		try
@@ -57,7 +138,7 @@ public class Fulfilment
 		//returns null as default
 		return null;
 	}
-	public Product[] returnProducts() throws Exception
+	private Product[] returnProducts() throws Exception
 	{
 		//Returns all products stored in data.json as product objects
 		try
