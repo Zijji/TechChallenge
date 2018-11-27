00ed0c34295447c60dc3053f4328c8365792de63
Edward <eingle101@gmail.com>
2018-11-27 18:47:19 +1100

    Converted JSON to java classes

--- apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/Testfulfilment.java
+++ apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/Testfulfilment.java
@@ -5,10 +5,36 @@ public class Testfulfilment
 		Fulfilment thisFulfilment = new Fulfilment();
 		try
 		{
+			System.out.println("Products");
 			Product[] getProducts = thisFulfilment.returnProducts();
 			for(int i = 0; i < getProducts.length; i++)
 			{
+				System.out.println("---");
 				System.out.println(getProducts[i].getProductId());
+				System.out.println(getProducts[i].getDescription());
+				System.out.println(getProducts[i].getQuantityOnHand());
+				System.out.println(getProducts[i].getReorderThreshold());
+				System.out.println(getProducts[i].getReorderAmount());
+				System.out.println(getProducts[i].getDeliveryLeadTime());
+			}
+			System.out.println("Orders");
+			Order[] getOrders = thisFulfilment.returnOrders();
+			for(int i = 0; i < getOrders.length; i++)
+			{
+				System.out.println("---");
+				System.out.println(getOrders[i].getOrderId());
+				System.out.println(getOrders[i].getStatus());
+				System.out.println(getOrders[i].getDateCreated());
+				Item[] getItemArray = getOrders[i].getItems();
+				for(int i2 = 0; i2 < getItemArray.length; i2++){
+					System.out.print(">Item ");
+					System.out.println(i2);
+					System.out.println(getItemArray[i2].getOrderId());
+					System.out.println(getItemArray[i2].getProductId());
+					System.out.println(getItemArray[i2].getQuantity());
+					System.out.println(getItemArray[i2].getCostPerItem());
+				}
+				
 			}
 		}
 		catch(Exception e)
--- apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/fulfilment.java
+++ apache-tomcat-8.5.29/webapps/api/v1/warehouse/fulfilment/fulfilment.java
@@ -7,6 +7,57 @@ import org.json.simple.parser.JSONParser;
 
 public class Fulfilment
 {
+	public Order[] returnOrders() throws Exception
+	{
+		//Returns all orders stored in data.json as Order objects
+		try
+		{
+			JSONParser parser = new JSONParser();
+			JSONObject getData = (JSONObject) parser.parse(new FileReader("data.json"));
+			JSONArray jsonproducts = (JSONArray) getData.get("orders");
+			
+			ArrayList<Order> orderList = new ArrayList<Order>();
+			
+			 
+			for(Object getObject : jsonproducts)
+			{
+				//Creates objects and stores them in orderlist
+				JSONObject getJSONObject = (JSONObject) getObject;
+				
+				long orderId = (long) getJSONObject.get("orderId");
+				String status = (String) getJSONObject.get("status");
+				String dateCreated = (String) getJSONObject.get("dateCreated");
+				//Gets items
+				JSONArray jsonitems = (JSONArray) getJSONObject.get("items");
+				
+				ArrayList<Item> itemList = new ArrayList<Item>();
+				for(Object getItemObject : jsonitems)
+				{	
+					JSONObject getJSONItemObject = (JSONObject) getItemObject;
+					long itemOrderId = (long) getJSONItemObject.get("orderId");
+					long productId = (long) getJSONItemObject.get("productId");
+					long quantity = (long) getJSONItemObject.get("quantity");
+					double costPerItem = (double) getJSONItemObject.get("costPerItem");
+					
+					Item getItem = new Item(itemOrderId, productId, quantity, costPerItem);
+					itemList.add(getItem);
+				}
+				Item[] items = itemList.toArray(new Item[itemList.size()]);
+				
+				Order getOrder = new Order(orderId, status, dateCreated, items);
+				orderList.add(getOrder);
+			}
+			
+			//Stores orderList as an array and returns this array.
+			return orderList.toArray(new Order[orderList.size()]);
+		}
+		catch(Exception e )
+		{
+			e.printStackTrace();
+		}
+		//returns null as default
+		return null;
+	}
 	public Product[] returnProducts() throws Exception
 	{
 		//Returns all products stored in data.json as product objects
